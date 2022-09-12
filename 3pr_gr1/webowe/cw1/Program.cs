073f28f4e1cece45277a21463c1ba5f17d37150e
pszczesz <pszczesz@gmail.com>
2022-09-12 16:10:34 +0200

    Update Program.cs

--- 3pr_gr1/webowe/cw1/Program.cs
+++ 3pr_gr1/webowe/cw1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Podaj imie: ");
+string? firstName=Console.ReadLine();
+Console.Write("Podaj nazwisko: ");
+string? lastName=Console.ReadLine();
+Console.Write("Podaj wiek: ");
+int age = Convert.ToInt32(Console.ReadLine());
+var wynik = age>=18 ? "pelnoletni" : "niepelnoletni";
+Console.WriteLine(
+@$"Witaj {firstName} {lastName} {wynik} dzisiaj jest: 
+       {DateTime.Now.ToShortDateString()}");
