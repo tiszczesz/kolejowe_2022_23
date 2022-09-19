9794a7e0e7a898db0472e50a26141deae608f6b3
pszczesz <pszczesz@gmail.com>
2022-09-19 15:13:15 +0200

    gg

---
+++ 3pr_gr1/webowe/cw2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("podaj a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("podaj b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("wybierz dzialanie (+,-,*,/): ");
+char doIt = Console.ReadLine()[0];
+if(doIt=='+'){
+    Console.WriteLine($"{a} + {b} = {a+b}");
+}else if(doIt=='-'){
+    Console.WriteLine($"{a} - {b} = {a-b}");
+}else if(doIt=='*'){
+    Console.WriteLine($"{a} * {b} = {a*b}");
+}else if(doIt=='/'){
+    var wynik = b!=0 ? ((double)a/b).ToString("0.00")
+         : "nie dzielimy przez zero";
+    Console.WriteLine($"{a} / {b} = {wynik}");
+}else{
+    Console.WriteLine("Złe działanie!!!");
+}
