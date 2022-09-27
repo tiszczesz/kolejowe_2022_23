18746c185c4cbd02554ff513a1445a1aaa0b7850
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-09-27 09:55:28 +0200

    gg

---
+++ kl2pa_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Podaj rozmiar tablicy: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = GenerRandom(size);
+ShowArray(numbers);
+int[] GenerRandom(int size){
+    Random rnd = new Random();
+    int[] result = new int[size];
+    for(int i=0;i<size;i++){
+        result[i] = rnd.Next(0,100);
+    }
+    return result;
+}
+void ShowArray(int[] data){
+    foreach(int elem in data){
+        Console.Write(elem+" ");
+    }
+}
+//zwraca indeks znalezionego elementu lub -1 jak go brak
