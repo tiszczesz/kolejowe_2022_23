169a910a8397f9b98ce60275a1c8731df574779a
tiszczesz <tiszczesz@users.noreply.github.com>
2022-09-27 08:05:04 +0200

    ff

---
+++ 3pr_gr2/cw1/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] names = {"Ala","Bella","Tomasz","Roman","Teresa","Tadeusz","tttt"};
+
+ShowAll(names);
+Console.WriteLine(" =========================================");
+ShowAll(ShowOnlyStartWith(names,"t").ToArray());
+
+
+
+//tools
+void ShowAll(string[] data){
+    foreach(var elem in data){
+        Console.WriteLine(elem);
+    }
+}
+List<string> ShowOnlyStartWith(string[] data,string c){
+    List<string> result = new List<string>();
+    foreach(var elem in data){
+        if( elem.ToLower().StartsWith( c.ToLower())){
+            result.Add(elem);
+        }
+    }
+    return result;
+}
+
