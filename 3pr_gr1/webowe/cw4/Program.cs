7c675ba6e8f8bff59a50063a840c7770db683bf1
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-10-03 15:24:25 +0200

    gg

--- 3pr_gr1/webowe/cw4/Program.cs
+++ 3pr_gr1/webowe/cw4/Program.cs
@@ -1,4 +1,11 @@
 ﻿User u1 = new User();
 User u2 = new User("Tomasz","qaz");
-u2.login = "fffff";
-Console.WriteLine("....");
+// u2.Login = "fffff";
+Console.WriteLine(u1.Login);
+Console.Write($"{u1.Login} czy chcesz zmienic haslo: (t/n) ");
+char isChange = Console.ReadLine()[0];
+if(Char.ToLower(isChange)=='t'){
+    Console.Write("Podaj haslo: ");
+    u1.Password = Console.ReadLine();
+}
+Console.WriteLine(u1);
--- 3pr_gr1/webowe/cw4/User.cs
+++ 3pr_gr1/webowe/cw4/User.cs
@@ -2,7 +2,15 @@
 class User
 {
     private string login = "noname";
+    public string Login{
+        get{return login.ToUpper();}
+        private set{login=value;}
+    }
     private string password = "noname";
+    public string Password{
+         get{return password;}
+        set{password = value;}
+    }
     public User()
     {
         login = "z konstuktora";
@@ -11,6 +19,10 @@ class User
     public User(string login,string password){
         this.login = login;
         this.password = password;
+    }
+    public override string ToString()
+    {
+        return base.ToString()+ $" ---->login: {login} haslo: {password}";
     }
 }
 
