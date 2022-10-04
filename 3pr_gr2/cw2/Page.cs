ecb802b0c8806c0e362c8bc9c7a1b11a7c79fc64
tiszczesz <tiszczesz@users.noreply.github.com>
2022-10-04 08:11:25 +0200

    hh

--- 3pr_gr2/cw2/Page.cs
+++ 3pr_gr2/cw2/Page.cs
@@ -1,8 +1,8 @@
 class Page{
     private string title;
     public string Title{
-        get{return title;}
-        set{title = value;}
+        get{return title.ToUpper();}
+       private set{title = value;}
     }
     private string content;
     public string Content{
@@ -12,14 +12,14 @@ class Page{
     public Page()
     {
         Title = "Noname";
-        Content = "Hello world";
+        Content = "Hello world";        
     }
-    public Page(string title,string content){
+    public Page(string title,string content,string author = "user"){
         Title = title;
         Content = content;
     }
     public override string ToString(){
-        return base.ToString();
+        return $"tytul: {Title} zawartosc: {Content} autor: {Author}";
     }
-    
+    public string Author { get; set; } = "user";
 }
--- 3pr_gr2/cw2/Program.cs
+++ 3pr_gr2/cw2/Program.cs
@@ -1,4 +1,5 @@
 ﻿Page p1 = new Page();
+Page p2 = new Page("Fajana strona","to jest ciekawa zawartosc strony");
 Console.WriteLine(p1.ToString());
-p1.Title = "dddddd";
-Console.WriteLine(p1.ToString());
+//p1.Title = "dddddd";
+Console.WriteLine(p2.ToString());
