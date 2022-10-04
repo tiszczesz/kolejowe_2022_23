50a6c565e3323e2b73051d513fe6ffa002742112
tiszczesz <tiszczesz@users.noreply.github.com>
2022-10-04 07:49:06 +0200

    Update Page.cs

--- 3pr_gr2/cw2/Page.cs
+++ 3pr_gr2/cw2/Page.cs
@@ -4,5 +4,22 @@ class Page{
         get{return title;}
         set{title = value;}
     }
-    string content;
+    private string content;
+    public string Content{
+        get{return content;}
+        set{content=value;}
+    }
+    public Page()
+    {
+        Title = "Noname";
+        Content = "Hello world";
+    }
+    public Page(string title,string content){
+        Title = title;
+        Content = content;
+    }
+    public override string ToString(){
+        return base.ToString();
+    }
+    
 }
