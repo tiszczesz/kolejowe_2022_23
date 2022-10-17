fe180a7824069681d4bc1ce14ea49afd58b0f193
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-10-17 18:47:25 +0200

    gg

--- zastepstwa/mymvc/Controllers/StudentController.cs
+++ zastepstwa/mymvc/Controllers/StudentController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using mymvc.Data;
+using mymvc.Models;
 
 namespace mymvc.Controllers
 {
     public class StudentController : Controller
     {
+        MyDbContext _db;
+        public StudentController(MyDbContext context)
+        {
+            _db = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var students = _db.Students.ToList();
+            return View(students);
         }
     }
 }
