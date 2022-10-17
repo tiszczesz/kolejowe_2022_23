18cc742e170a7ed21671dfec086b3e7d4ce0ef82
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-10-17 18:16:39 +0200

    gg

---
+++ zastepstwa/mymvc/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mymvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
