d736940131aa33f4b2945f1b45451aaa7c267e90
tiszczesz <tiszczesz@users.noreply.github.com>
2022-11-29 05:42:46 +0100

    Update RepoGames.cs

--- 3pr_gr2/WebRazorPageGames/WebRazorPageGames/Models/RepoGames.cs
+++ 3pr_gr2/WebRazorPageGames/WebRazorPageGames/Models/RepoGames.cs
@@ -37,8 +37,24 @@
                     Id = 4,
                     Title = "Diablo 3",
                     Category = "RPG" ,
-                    ReleaseDate = new DateTime(2016, 3, 12)
+                    ReleaseDate = new DateTime(2021, 3, 12)
                 });
+            Games.Add(
+               new Game
+               {
+                   Id = 5,
+                   Title = "Jaś i Małgosia 3",
+                   Category = "RPG",
+                   ReleaseDate = new DateTime(2016, 3, 12)
+               });
+            Games.Add(
+               new Game
+               {
+                   Id = 6,
+                   Title = "Diablo 4 beta",
+                   Category = "RPG",
+                   ReleaseDate = new DateTime(2022, 3, 12)
+               });
         }
     }
 }
