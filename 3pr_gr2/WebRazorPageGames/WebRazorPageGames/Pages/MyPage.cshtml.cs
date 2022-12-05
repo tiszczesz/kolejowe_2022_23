74d168ecbeec63b402af2d23a90c992466aa4c3f
tiszczesz <tiszczesz@users.noreply.github.com>
2022-12-05 16:24:02 +0100

    ff

---
+++ 3pr_gr2/WebRazorPageGames/WebRazorPageGames/Pages/MyPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazorPageGames.Pages
+{
+    public class MyPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
