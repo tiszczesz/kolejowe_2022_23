16779749f642b23934dd78ecb2cefe4098f82234
tiszczesz <tiszczesz@users.noreply.github.com>
2022-12-13 04:31:59 +0100

    dd

--- 3pr_gr2/cw6_RazorWithModel/cw6_RazorWithModel/Models/Book.cs
+++ 3pr_gr2/cw6_RazorWithModel/cw6_RazorWithModel/Models/Book.cs
@@ -10,12 +10,16 @@ namespace cw6_RazorWithModel.Models
         public int Id { get; set; }
 
         [DisplayName("Tytuł książki")]
+        [Required(ErrorMessage ="Proszę podać tytuł")]
+        [MinLength(2,ErrorMessage ="Tyuł musi posiadać minimum 2 znaki")]
         public string Title { get; set; }
         [DisplayName("Autor książki")]
+        [Required(ErrorMessage = "Proszę podać autora")]
         public string Author { get; set; }
 
         [DisplayName("Data wydania")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Proszę wybrać datę wydania")]
         public DateOnly Date { get; set; }
     }
 }
