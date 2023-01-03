9a4f55e86f0db0b6e2b24aebceaaa7dab8927110
tiszczesz <tiszczesz@users.noreply.github.com>
2023-01-03 05:57:03 +0100

    fff

---
+++ 3pr_gr2/WebRazorSelect/WebRazorSelect/Models/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebRazorSelect.Models
+{
+    public class Worker
+    {
+        [Required(ErrorMessage = "Podaj imię")]
+        [DisplayName("Podaj imię")]
+        [MinLength(3,ErrorMessage = "Muszą być 3 litery")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Podaj nazwisko")]
+        [DisplayName("Podaj nazwisko")]
+        [MinLength(3, ErrorMessage = "Muszą być 3 litery")]
+        public string? LastName { get; set; }
+
+        [DisplayName("Miejsce pracy")]
+        public string Workplace { get; set; }
+
+        [DisplayName("Pensja")]
+        [DataType(DataType.Currency)]
+        public double Salary { get; set; }
+    }
+}
---
+++ 3pr_gr2/WebRazorSelect/WebRazorSelect/Pages/AddWorker.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebRazorSelect.Models;
+
+namespace WebRazorSelect.Pages
+{
+    public class AddWorkerModel : PageModel
+    {
+        [BindProperty]
+        public Worker MyWorker { get; set; }
+        public List<string> WorkerPlaces { get; set; }
+        public AddWorkerModel()
+        {
+            WorkerPlaces = new List<string>
+            {
+                "Asystent/ka","handlowiec","kierowca","sprz¹tacz/ka","kierownik"
+            };
+        }
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var result = MyWorker;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
