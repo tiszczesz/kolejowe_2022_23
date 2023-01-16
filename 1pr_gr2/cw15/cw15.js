135c306f7b282576afc99d4102395a5f431f6eb9
tiszczesz <tiszczesz@users.noreply.github.com>
2023-01-17 00:39:55 +0100

    Update cw15.js

--- 1pr_gr2/cw15/cw15.js
+++ 1pr_gr2/cw15/cw15.js
@@ -8,3 +8,19 @@ TimeUpdate(timer);
 setInterval(function () {
     TimeUpdate(timer);
 }, 1000);
+
+const months = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec",
+    "lipiec", "sierpień", "wrzesień",
+    "październik", "listopad", "grudzień"];
+const days = ["niedziela", "poniedziałek", "wtorek",
+    "środa", "czwartek", "piątek", "sobota"];
+
+function DateUpdate(){
+    const currentDate = new Date();
+    document.querySelector(".year").innerHTML = currentDate.getFullYear();
+    document.querySelector(".date").innerHTML = currentDate.getDate();
+    document.querySelector(".month").innerHTML = months[currentDate.getMonth()];
+    document.querySelector(".day").innerHTML = days[currentDate.getDay()];
+}
+
+DateUpdate();
