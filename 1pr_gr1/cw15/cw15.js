5a67d333204f6e951790b2745f10c3ac8c0b6467
tiszczesz <tiszczesz@users.noreply.github.com>
2023-01-16 23:56:53 +0100

    gg

--- 1pr_gr1/cw15/cw15.js
+++ 1pr_gr1/cw15/cw15.js
@@ -1,10 +1,25 @@
-function showTime(elem){
+function showTime(elem) {
     const time = new Date();
     elem.innerHTML = time.toLocaleTimeString();
 }
 
 const timer = document.querySelector(".timer");
 showTime(timer);
-setInterval(function(){
+setInterval(function () {
     showTime(timer);
-},1000);
+}, 1000);
+const days = ["niedziela", "poniedziałek", "wtorek", "środa", "czwartek",
+    "piątek", "sobota"];
+const months = ["styczeń", "luty", "marzec", "kwiecień", "maj",
+    "czerwiec", "lipiec", "sierpień", "wrzesień", "październik",
+    "listopad", "grudzień"];
+function setDate() {
+    const currentDate = new Date();
+    document.querySelector(".year").innerHTML = currentDate.getFullYear();
+    document.querySelector(".month")
+        .innerHTML = months[currentDate.getMonth()];
+    document.querySelector(".date").innerHTML = currentDate.getDate();
+    document.querySelector(".day")
+        .innerHTML = days[currentDate.getDay()];
+}
+setDate();
