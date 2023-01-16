4e05c9dc9ab183fb0af62dd91382c00afb30966b
tiszczesz <tiszczesz@users.noreply.github.com>
2023-01-16 22:43:12 +0100

    gg

---
+++ zastepstwa/cpp1/napisy/napisy/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void SortStrings(string t[],int size){
+    return;
+}
+int main()
+{
+    string words[] = {"ala","monika","edyta","halina","elzbieta","renata"};
+    for(auto a : words){
+        cout << a << endl;
+    }
+    SortStrings(words,6);
+     for(auto a : words){
+        cout << a << endl;
+    }
+    cout << " ----------------------------------------- "<<endl;
+    if(words[0]>words[1]){
+        cout << words[0] << endl;
+    }else{
+        cout << words[1] << endl;
+    }
+    return 0;
+}
