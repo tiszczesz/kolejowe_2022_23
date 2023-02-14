6fd7bccec94ab13e6ae393120d0304320f22a274
pszczesz <pszczesz@gmail.com>
2023-02-14 10:37:32 +0100

    dd

--- zastepstwa/MinimalApi/MinimalApi/Models/TodoDb.cs
+++ zastepstwa/MinimalApi/MinimalApi/Models/TodoDb.cs
@@ -9,5 +9,14 @@ namespace MinimalApi.Models
         }
 
         public DbSet<Todo> Todos => Set<Todo>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Todo>().HasData(new Todo{Id=1,Name = "Wyjść z psem",IsComplete = false});
+            modelBuilder.Entity<Todo>().HasData(new Todo{Id=2,Name = "Zjeść obiad",IsComplete = false});
+            modelBuilder.Entity<Todo>().HasData(new Todo{Id=3,Name = "Ubrać się",IsComplete = false});
+            modelBuilder.Entity<Todo>().HasData(new Todo{Id=4,Name = "Tralalala",IsComplete = false});
+            modelBuilder.Entity<Todo>().HasData(new Todo{Id=5,Name = "Inne sprawy",IsComplete = false});
+        }
     }
 }
--- zastepstwa/MinimalApi/MinimalApi/Program.cs
+++ zastepstwa/MinimalApi/MinimalApi/Program.cs
@@ -7,9 +7,24 @@ builder.Services.AddDbContext<TodoDb>
     (opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
-app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
+app.MapGet("/todoitems", async (TodoDb db) =>
+{
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) => {
+    var todos = await db.Todos.ToListAsync();
+    if (todos.Count == 0)
+    {
+        db.Todos.Add(new Todo { Id = 1, Name = "Wyjść z psem", IsComplete = false });
+        db.Todos.Add(new Todo { Id = 2, Name = "Inna czynność", IsComplete = false });
+        db.Todos.Add(new Todo { Id = 3, Name = "Rybki nakarmić", IsComplete = false });
+        db.Todos.Add(new Todo { Id = 4, Name = "Dobrze zjeść", IsComplete = false });
+        db.SaveChanges();
+        todos = await db.Todos.ToListAsync();
+    }
+    return todos;
+});
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
     return Results.Created($"/todoitems/{todo.Id}", todo);
