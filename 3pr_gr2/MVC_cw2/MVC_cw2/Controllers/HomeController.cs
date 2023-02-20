35c6128e1d6ee6bbde8ea2f1f3ec9aec1af38e42
tiszczesz <tiszczesz@users.noreply.github.com>
2023-02-20 22:05:22 +0100

    gg

---
+++ 3pr_gr2/MVC_cw2/MVC_cw2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_cw2.Models;
+using System.Diagnostics;
+
+namespace MVC_cw2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.controller = ControllerContext
+               .RouteData.Values["Controller"].ToString();
+            ViewBag.action = ControllerContext
+                .RouteData.Values["action"].ToString();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+  
+            return View();
+        }
+        public IActionResult Akcja2()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 3pr_gr2/MVC_cw2/MVC_cw2/Controllers/MyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_cw2.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
