e6ae9b622473d85b39999063e6aee4b4a48311e6
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-02-27 14:53:57 +0100

    ff

---
+++ 3pr_gr1/webowe/MVC_first_v2/MVC_first_v2/Models/NWD.cs
@@ -0,0 +1,21 @@
+﻿namespace MVC_first_v2.Models
+{
+	public class NWD
+	{
+		public int? A { get; set; }
+		public int? B { get; set; }
+
+		public int? Result { get; set; }
+
+		public int? NWDRec(int? a, int? b) {
+			if (b == 0) return a;
+			return NWDRec(b, a % b);
+		}
+
+		public void Count()
+		{
+			Result = NWDRec(A, B);
+		}
+
+	}
+}
---
+++ 3pr_gr1/webowe/MVC_first_v2/NWD.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace NWD.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
