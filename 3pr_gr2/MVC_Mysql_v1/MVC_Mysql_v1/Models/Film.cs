00a179dfc9bac6aacbd215b2ad7f57905bccd00e
tiszczesz <tiszczesz@users.noreply.github.com>
2023-03-20 05:09:04 +0100

    gg

--- 3pr_gr2/MVC_Mysql_v1/MVC_Mysql_v1/Models/Film.cs
+++ 3pr_gr2/MVC_Mysql_v1/MVC_Mysql_v1/Models/Film.cs
@@ -1,9 +1,16 @@
-﻿namespace MVC_Mysql_v1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Mysql_v1.Models
 {
     public class Film
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Musisz podać tytuł")]
+        [Display(Name = "Tytuł filmu")]
         public string? Title { get; set; }
+
+        [Range(minimum: 0, maximum: 1000)]
         public int? Time { get; set; }
         public DateTime? Date { get; set; }
     }
