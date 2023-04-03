9968e8fd34ed893b287dfaa5467ee18d092e2234
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-04-03 15:32:39 +0200

    ff

--- 3pr_gr1/webowe/MySql_v1_wycieczki/MySql_v1_wycieczki/Models/Trip.cs
+++ 3pr_gr1/webowe/MySql_v1_wycieczki/MySql_v1_wycieczki/Models/Trip.cs
@@ -1,10 +1,18 @@
-﻿namespace MySql_v1_wycieczki.Models
+﻿using System.ComponentModel;
+
+namespace MySql_v1_wycieczki.Models
 {
     public class Trip
     {
         public int Id { get; set; }
+
+        [DisplayName("Miejsce wycieczki")]
         public string? Place { get; set; }
+
+        [DisplayName("Cena")]
         public decimal? Price { get; set; }
+
+        [DisplayName("Data wycieczki")]
         public DateOnly? Date { get; set; }
 
     }
